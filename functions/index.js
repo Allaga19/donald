b8c199b01c1ad2ea95649fe42652d317f5a1b025
Allaga <Allaga59@yandex.ru>
2021-04-24 00:14:59 +0300

    com

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,7 @@
 const functions = require("firebase-functions");
+const nodemailer = require('nodemailer');
+const htmlToText = require('nodemailer-html-to-text').htmlToText;
+const { email, password } = require(''./config');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.sendUseEmail = functions.database.ref('orders/{pushID}')
+.onCreate(order:  => console.log(order));
